c8a56e2d3a4da4dc1029f10b0d4bef439eeffc61
Long Nguyen <longnt80@gmail.com>
2017-10-17 13:24:34 +0700

    FIxed bug in Pagination
    Fixed bug which let user click the Previous button or Next button when
    at the first page or last page which break the app.

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,33 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { currentPagination } from '../actions/index';
 
-const Pagination = ({numberOfPage, setPageNumber, currentPage}) => {
-	let pageNumberArray = [];
-	for (let i = 1; i <= numberOfPage; i++) {
-		pageNumberArray.push(i);
-	}
 
-	return (
-			<nav className="pagination is-small" aria-label="pagination">
-				<a onClick={() => setPageNumber(currentPage-1)} className="pagination-previous" title="This is the first page" disabled={currentPage === 1}>Previous</a>
-				<a onClick={() => setPageNumber(currentPage+1)} className="pagination-next" disabled={(numberOfPage === currentPage)}>Next page</a>
-				<ul className="pagination-list">
-					{
-						pageNumberArray.map(num => 
-							(
-								<li key={num}>
-										<a onClick={() => setPageNumber(num)} className={"pagination-link " + ((currentPage === num) ? 'is-current': '')} >
-											{num}
-										</a>
-								</li>
+class Pagination extends Component {
+
+	handlePrevPageBtn = () => {
+		const {setPageNumber, currentPage} = this.props;
+	
+		if ( currentPage === 1 ) {
+			return;
+		}
+		else {
+			setPageNumber(currentPage-1)
+		}
+	}
+	
+	handleNextPageBtn = () => {
+		const {numberOfPage, setPageNumber, currentPage} = this.props;
+	
+		if ( currentPage === numberOfPage ) {
+			return;
+		}
+		else {
+			setPageNumber(currentPage+1)
+		}
+	}
+	
+	render() {
+		const {numberOfPage, setPageNumber, currentPage} = this.props;
+
+		let pageNumberArray = [];
+		for (let i = 1; i <= numberOfPage; i++) {
+			pageNumberArray.push(i);
+		}
+	
+		return (
+				<nav className="pagination is-small" aria-label="pagination">
+					<a onClick={this.handlePrevPageBtn} className="pagination-previous" title="This is the first page" disabled={currentPage === 1}>Previous</a>
+					<a onClick={this.handleNextPageBtn} className="pagination-next" disabled={(numberOfPage === currentPage)}>Next page</a>
+					<ul className="pagination-list">
+						{
+							pageNumberArray.map(num => 
+								(
+									<li key={num}>
+											<a onClick={() => setPageNumber(num)} className={"pagination-link " + ((currentPage === num) ? 'is-current': '')} >
+												{num}
+											</a>
+									</li>
+								)
 							)
-						)
-					}
-				</ul>
-			</nav>
-	);
-};
+						}
+					</ul>
+				</nav>
+		);
+	}
+}
 
 const mapStateToProps = (state) => {
     return {
